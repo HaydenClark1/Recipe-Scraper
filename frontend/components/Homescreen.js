1741f6a10223d23ed870c35233c7c0a31f9c9760
haydenclark12 <hdclark2@wisc.edu>
2025-04-21 12:06:23 -0500

    eas build

--- frontend/components/Homescreen.js
+++ frontend/components/Homescreen.js
@@ -16,7 +16,7 @@ export default function Homescreen({navigation}){
 
 
   //const backendURL = "http://192.168.4.49:7000"
-  const backendURL = "https://recipe-backend.onrender.com";
+  const backendURL = "https://recipe-scraper-hk6l.onrender.com";
   const [URL,setURL] = useState("")
   const [recipeData,setRecipeData] = useState(null)
   const [modalVisible, setModalVisible] = useState(false);
@@ -75,15 +75,12 @@ export default function Homescreen({navigation}){
         body: JSON.stringify({url:URL}),
       });
       
-      const text = await response.text();
-      console.log("Raw response:", text); // <--- Add this!
-
-      /*const data = await response.json();
+      const data = await response.json();
       if (data){
         setRecipeData(data)
         setModalVisible(true)
         console.log(data)
-      }*/
+      }
      
 
 
